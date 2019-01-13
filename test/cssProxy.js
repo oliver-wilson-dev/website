d6f559401895d878fc85f45d28ece7f3938915c4
Ollie Wilson <oliver.wilson@asos.com>
2019-01-13 23:37:25 +0000

    feat: added jest

---
+++ test/cssProxy.js
@@ -0,0 +1,24 @@
+/*
+  Lovingly ripped-off from https://github.com/keyanzhang/identity-obj-proxy
+  Added benefit of being able to prototype values within tests to simulate
+  the @value export abilities of css modules.
+*/
+
+const idObj = new Proxy(
+  {},
+  {
+    get: function getter(target, key) {
+      if (key === '__esModule') {
+        return false;
+      }
+      if (target[key]) {
+        return target[key];
+      }
+
+      return key;
+    }
+  }
+);
+
+module.exports = idObj;
+
