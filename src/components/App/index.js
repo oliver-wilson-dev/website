9c295623e4837ee5f69d6316462d6db9dd614846
Ollie Wilson <oliver.wilson@asos.com>
2019-01-13 20:43:42 +0000

    feat: added eslint

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,10 @@
-import React, {Component } from "react";
-import styles from "./index.css";
+import React from 'react';
+import styles from './index.css';
 
-class App extends Component {
-    render() {
-        console.log(styles);
-        
-        return (
-            <div>
-                <h1 className={styles.title}>Hello world</h1>
-            </div>
-        )
-    }
-}
+const App = () => (
+  <div>
+    <h1 className={styles.title}>Hello world</h1>
+  </div>
+);
 
 export default App;
