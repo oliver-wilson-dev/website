99b497edfec3a43170c866ceab4e98031b839eb0
Ollie Wilson 👋 <Ollie>
2019-06-05 13:29:04 +0100

    refacor: update github account from work account to personal account

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ export default () => (
     <span className={styles.footerDisclaimer}>
 the code for this website can be found on
       {' '}
-      <a href="https://github.com/asos-oliverwilson/react-portfolio-website" target="_blank" rel="noopener noreferrer">github</a>
+      <a href="https://github.com/oliver-wilson-dev/oliver-wilson-dev.github.io" target="_blank" rel="noopener noreferrer">github</a>
 . ©2019, Oliver Wilson
     </span>
   </div>
--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -6,10 +6,10 @@ import sharedStyles from '../App/index.css';
 const SocialMedia = () => (
   <div className={cn(styles.socialContainer, sharedStyles.flexCenter)}>
     <a className={styles.socialLink} href="https://www.linkedin.com/in/oliver-wilson-profile/">
-      <img className={styles.socialLogo} src="public/img/linkedin.svg" alt="my linkedin profile" />
+      <img className={styles.socialLogo} src="public/img/linkedin.svg" alt="my linkedIn profile" />
     </a>
-    <a className={styles.socialLink} href="https://github.com/asos-oliverwilson">
-      <img className={styles.socialLogo} src="public/img/github.svg" alt="my linkedin profile" />
+    <a className={styles.socialLink} href="https://github.com/oliver-wilson-dev/">
+      <img className={styles.socialLogo} src="public/img/github.svg" alt="my linkedIn profile" />
     </a>
   </div>
 );
