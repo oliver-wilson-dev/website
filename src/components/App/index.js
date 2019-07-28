3e842009d6a160103c65574fcbe8e553876d4a11
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-07-28 23:15:12 +0100

    feat: created Sections container with some selectors for state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,7 @@ import styles from './index.css';
 import DownloadButton from '../DownloadButton';
 import Footer from '../Footer';
 import Landing from '../Landing';
-import Sections from '../Sections';
+import Sections from '../../containers/Sections';
 import SocialMedia from '../SocialMedia';
 import ToggleSwitch from '../../containers/ToggleSwitch';
 
