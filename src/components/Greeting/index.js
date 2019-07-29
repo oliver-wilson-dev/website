c04ca3a7e70c9d59823d4ec6d5a4ac404a10c883
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-07-29 21:53:44 +0100

    feat: greeting component

---
+++ src/components/Greeting/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Section from '../Section';
+import styles from './index.css';
+
+const Greeting = ({ title, content, openingLine }) => (
+  <Section title={title}>
+    <p>
+      <span className={styles.paragraphEmoji} role="img" aria-label="university emoji">👋</span>
+      {openingLine.start}
+      {' '}
+      {<span className={styles.strikethroughJoke}>{openingLine.joke}</span>}
+      {' '}
+      {openingLine.end}
+    </p>
+    {content.map(paragraph => <p key={paragraph}>{paragraph}</p>)}
+  </Section>
+);
+
+Greeting.propTypes = {
+  content: PropTypes.array.isRequired,
+  openingLine: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Greeting;
