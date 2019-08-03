328c2e97af9351b78ece86d57790386ada8ec076
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-08-03 15:42:48 +0100

    feat: fetchContent action

--- src/state/actions/constants.js
+++ src/state/actions/constants.js
@@ -1,3 +1,6 @@
+export const SECTIONS_API_ENDPOINT = 'https://express.oliver-wilson-dev.now.sh/sections';
+
+// ACTION TYPES
 export const TOGGLE_THEME = 'TOGGLE_THEME';
 export const FETCH_CONTENT = 'FETCH_CONTENT';
 export const FETCH_CONTENT_FAILURE = 'FETCH_CONTENT_FAILURE';
