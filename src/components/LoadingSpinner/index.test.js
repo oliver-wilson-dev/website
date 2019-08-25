97a6f65372c786601f22be2bc307553f17a258c8
Oliver Wilson <contact.oliver.wilson@gmail.com>
2019-08-25 22:35:55 +0100

    Feat/spinner debounce (#12)
    feat: update loading spinner to respect minimum transition time

--- src/components/LoadingSpinner/index.test.js
+++ src/components/LoadingSpinner/index.test.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import LoadingSpinner from '.';
+import styles from './index.css';
 
-const render = () => shallow(<LoadingSpinner />);
+const defaultProps = {
+  loading: true
+};
+
+const render = overrideProps => shallow(<LoadingSpinner {...defaultProps} {...overrideProps} />);
 
 describe('<LoadingSpinner />', () => {
   it('should render correctly', () => {
     expect(render().exists()).toBe(true);
     expect(render()).toMatchSnapshot();
+  });
+
+  describe('when the component is not loading', () => {
+    it('should render correctly', () => {
+      const component = render();
+      component.setProps({ loading: false });
+      component.find(`.${styles.loadingSpinner}`).simulate('transitionEnd');
+      expect(component).toMatchSnapshot();
+    });
+
+    it('should add the hide class to the loadingSpinner div', () => {
+      const component = render();
+      component.setProps({ loading: false });
+      expect(component.find(`.${styles.hide}`).exists()).toBe(true);
+    });
   });
 });
