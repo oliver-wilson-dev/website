aa3dfcc13a95d90b5c16c67415df16cb63778c81
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-08-25 21:36:11 +0100

    feat: update loading spinner to respect minimum transition time

--- src/components/LoadingSpinner/index.js
+++ src/components/LoadingSpinner/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 import styles from './index.css';
 
-const LoadingSpinner = () => (
-  <div className={styles.loadingSpinner} />
-);
 
+class LoadingSpinner extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      transitionDone: false
+    };
+  }
+
+  onTransitionEnd() {
+    this.setState({ transitionDone: true });
+  }
+
+  render() {
+    const { props: { loading }, state: { transitionDone } } = this;
+
+    return transitionDone
+      ? null : (
+        <div className={styles.wrapper}>
+          <div
+            className={classnames(styles.loadingSpinner, {
+              [styles.hide]: !loading
+            })}
+            onTransitionEnd={() => this.onTransitionEnd()}
+          />
+        </div>
+      );
+  }
+}
+
+LoadingSpinner.propTypes = {
+  loading: PropTypes.bool.isRequired
+};
 
 export default LoadingSpinner;
