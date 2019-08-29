714e3de87d05da627fbf0a450573aa40451d2afd
Oliver Wilson <contact.oliver.wilson@gmail.com>
2019-08-29 20:00:36 +0100

    Feature/store theme in cookie value (#16)

---
+++ src/state/reducers/theme/cookieUtilities/constants.js
@@ -0,0 +1,2 @@
+export const COOKIE_MAX_AGE_TO_ERASE = 'Max-Age=-99999999';
+export const COOKIE_PATH = 'path=/';
---
+++ src/state/reducers/theme/index.js
@@ -0,0 +1,14 @@
+import { TOGGLE_THEME } from '../../actions/constants';
+import toggleTheme from './reduceCases/toggleTheme';
+import getInitialState from './getInitialState';
+
+
+const themeReducer = (state = getInitialState(), action) => new Proxy({
+  [TOGGLE_THEME]: toggleTheme({ state }),
+}, {
+  get: (reduceCases, actionType) => (actionType in reduceCases
+    ? reduceCases[actionType]()
+    : state)
+})[action.type];
+
+export default themeReducer;
