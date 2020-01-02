69147b81025487d61ecc78c40625693d4b4b079b
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-01-02 22:09:17 +0000

    Update copyright to 2020 (#39)

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,10 +7,10 @@ import styles from './index.css';
 const Footer = () => (
   <div className={cn(additionalStyles.flexColumn, additionalStyles.flexCenter)}>
     <span className={styles.footerDisclaimer}>
-the code for this website can be found on
+The code for this website can be found on
       {' '}
       <a href="https://github.com/oliver-wilson-dev/oliver-wilson-dev.github.io" target="_blank" rel="noopener noreferrer">github</a>
-. ©2019, Oliver Wilson
+. ©2020, Oliver Wilson
     </span>
   </div>
 );
