c724ea1823c3b42cf4a6aff12d29b2951b9ec44d
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-01-03 11:24:39 +0000

    refactor: introudce the useState hook to the LoadingSpinner component

--- src/components/LoadingSpinner/index.js
+++ src/components/LoadingSpinner/index.js
@@ -1,38 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import styles from './index.css';
 
+const LoadingSpinner = ({ loading }) => {
+  const [transitionDone, setTransitionDone] = useState(false);
 
-class LoadingSpinner extends React.Component {
-  constructor(props) {
-    super(props);
+  const onTransitionEnd = () => setTransitionDone(true);
 
-    this.state = {
-      transitionDone: false
-    };
-  }
-
-  onTransitionEnd() {
-    this.setState({ transitionDone: true });
-  }
-
-  render() {
-    const { props: { loading }, state: { transitionDone } } = this;
-
-    return transitionDone
-      ? null : (
-        <div className={styles.wrapper}>
-          <div
-            className={classnames(styles.loadingSpinner, {
-              [styles.hide]: !loading
-            })}
-            onTransitionEnd={() => this.onTransitionEnd()}
-          />
-        </div>
-      );
-  }
-}
+  return transitionDone
+    ? null : (
+      <div className={styles.wrapper}>
+        <div
+          className={classnames(styles.loadingSpinner, {
+            [styles.hide]: !loading
+          })}
+          onTransitionEnd={onTransitionEnd}
+        />
+      </div>
+    );
+};
 
 LoadingSpinner.propTypes = {
   loading: PropTypes.bool.isRequired
