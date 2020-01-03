456e18eb3925694a9555711136a05befa71d1d05
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-01-03 15:17:28 +0000

    refactor: introduce the useEffect hook to the Sections component

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Greeting from '../Greeting';
 import Education from '../Education';
@@ -6,32 +6,26 @@ import Experience from '../Experience';
 import LoadingSpinner from '../LoadingSpinner';
 import Contact from '../Contact';
 
-
-class Sections extends React.Component {
-  componentDidMount() {
-    const { props: { fetchContent } } = this;
+const Sections = ({ fetchContent, sections, sectionsContentFetched }) => {
+  useEffect(() => {
     fetchContent();
-  }
+  }, []);
 
-  render() {
-    const { props: { sections, sectionsContentFetched } } = this;
-
-    return (
-      <React.Fragment>
-        <LoadingSpinner loading={!sectionsContentFetched} />
-        {sectionsContentFetched && (
+  return (
+    <React.Fragment>
+      <LoadingSpinner loading={!sectionsContentFetched} />
+      {sectionsContentFetched && (
         <React.Fragment>
           <Greeting {...sections.greeting} />
           <Education {...sections.education} />
           <Experience {...sections.experience} />
           <Contact {...sections.contact} />
         </React.Fragment>
-        )}
+      )}
 
-      </React.Fragment>
-    );
-  }
-}
+    </React.Fragment>
+  );
+};
 
 Sections.propTypes = {
   fetchContent: PropTypes.func.isRequired,
