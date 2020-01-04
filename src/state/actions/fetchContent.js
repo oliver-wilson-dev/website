f688e58907afa56f84bcbb9f5aa6845664c4520f
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-01-04 14:18:27 +0000

    feat: name fetchContent instead of having it as a default export

--- src/state/actions/fetchContent.js
+++ src/state/actions/fetchContent.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { FETCH_CONTENT, FETCH_CONTENT_FAILURE, SECTIONS_API_ENDPOINT } from './constants';
 
-export default () => async (dispatch) => {
+const fetchContent = () => async (dispatch) => {
   try {
     const { data } = await axios.get(SECTIONS_API_ENDPOINT);
 
@@ -10,3 +10,5 @@ export default () => async (dispatch) => {
     dispatch({ type: FETCH_CONTENT_FAILURE, error });
   }
 };
+
+export default fetchContent;
