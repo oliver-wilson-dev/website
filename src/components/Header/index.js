3aeabbb3a2e80e080f72b2b9c17f5d8bee61b126
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-06-15 22:56:26 +0100

    feat: open and close sidenav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import cn from 'classnames';
 import styles from './index.css';
 import ThemeToggle from '../../containers/ThemeToggle';
+import BurgerMenu from './burgerMenu.svg';
+import CrossIcon from '../Icons/cross.svg';
+import { preventScroll, allowScroll } from '../../utils';
 
-const Header = () => (
-  <nav
-    className={cn(styles.header)}
-  >
-    <div className={cn(styles.wrapper)}>
-      <ThemeToggle />
-    </div>
-  </nav>
-);
+const Header = () => {
+  const [open, setOpen] = useState(false);
+  const [close, setClose] = useState(false);
+  const isFirstRender = useRef(true);
+
+  const toggleMenu = () => {
+    const burgerMenuNotYetClicked = !open && !close;
+
+    if (burgerMenuNotYetClicked) {
+      setOpen(true);
+      setClose(false);
+    } else {
+      setOpen(!open);
+      setClose(!close);
+    }
+  };
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+    } else if (open) {
+      preventScroll();
+    } else { allowScroll(); }
+  }, [open]);
+
+  return (
+    <nav
+      className={cn(styles.header)}
+    >
+      <div className={cn(styles.wrapper)}>
+        <ThemeToggle />
+        <BurgerMenu className={styles.burgerMenu} onClick={toggleMenu} />
+      </div>
+      <div
+        className={cn(styles.menuContainer, {
+          [styles.menuOpen]: open,
+          [styles.menuClose]: close && !open,
+          [styles.menuHideFirstRender]: isFirstRender.current
+        })}
+      >
+        <div
+          className={styles.menuContent}
+        >
+          <CrossIcon
+            className={cn(styles.crossIcon, {
+              [styles.crossIconShow]: open
+            })}
+            onClick={toggleMenu}
+          />
+          <ul className={styles.menuList}>
+            <li className={styles.menuItem}>
+              Home
+            </li>
+            <li className={styles.menuItem}>
+              About
+            </li>
+            <li className={styles.menuItem}>
+              Education
+            </li>
+            <li className={styles.menuItem}>
+              Experience
+            </li>
+          </ul>
+        </div>
+
+
+      </div>
+    </nav>
+  );
+};
 
 
 export default Header;
--- src/components/Header/index.test.js
+++ src/components/Header/index.test.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import Header from './index';
+import styles from './index.css';
+import { preventScroll, allowScroll } from '../../utils';
+
+jest.mock('../../utils', () => ({
+  preventScroll: jest.fn(),
+  allowScroll: jest.fn()
+}));
+
+jest.mock('../Icons/cross.svg', () => {
+  const CrossIcon = () => null;
+
+  return CrossIcon;
+});
+
+jest.mock('./burgerMenu.svg', () => {
+  const BurgerMenu = () => null;
+
+  return BurgerMenu;
+});
 
 jest.mock('../../containers/ThemeToggle', () => {
   const ThemeToggle = () => null;
@@ -10,14 +30,61 @@ jest.mock('../../containers/ThemeToggle', () => {
 
 const defaultProps = { theme: 'test-theme' };
 
-const render = overrideProps => shallow(<Header {...defaultProps} {...overrideProps} />);
+const render = (
+  overrideProps,
+  renderMethod = shallow
+) => renderMethod(<Header {...defaultProps} {...overrideProps} />);
 
 describe('<Header/> component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+
   it('should exist', () => {
     expect(render().exists()).toBe(true);
   });
 
   it('should render correctly', () => {
     expect(render()).toMatchSnapshot();
+  });
+
+  describe('when clicking the burgerMenu icon', () => {
+    it('should open the sub navigation', () => {
+      const component = render();
+
+      component.find(`.${styles.burgerMenu}`).simulate('click');
+
+      expect(component).toMatchSnapshot();
+    });
+
+    it('should call preventScroll', () => {
+      const component = render({}, mount);
+
+      act(() => {
+        component.find(`.${styles.burgerMenu}`).props().onClick();
+      });
+
+      expect(preventScroll).toHaveBeenCalledTimes(1);
+    });
+
+    describe('and then clicking the close icon', () => {
+      it('should call allowScroll', () => {
+        const component = render({}, mount);
+
+        // can't click on the svg element in jest for some reason
+        act(() => {
+          component.find(`.${styles.burgerMenu}`).props().onClick();
+        });
+
+        component.update();
+
+        act(() => {
+          component.find(`.${styles.crossIcon}`).props().onClick();
+        });
+
+        expect(allowScroll).toHaveBeenCalledTimes(1);
+      });
+    });
   });
 });
