146c1c6b809eb2dab3f7951f2e2314e36d22f72f
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-06-16 18:15:00 +0100

    feat: use shared styles for header on landing

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import cn from 'classnames';
 import styles from './index.css';
+import sharedStyles from '../../styles/shared.css';
 
-export default () => (
+const Landing = () => (
   <div className={cn(styles.container, styles.landing)}>
-    <h1 className={styles.landingHeader}>Oliver Wilson</h1>
+    <h1 className={sharedStyles.pageHeader}>Oliver Wilson</h1>
     <h2 className={styles.landingSubHeader}>
       Web developer
     </h2>
   </div>
 );
+
+export default Landing;
