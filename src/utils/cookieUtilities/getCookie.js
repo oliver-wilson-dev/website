838b0ab5de7b1ee7d27c9996b4bcbe7db34a3aaa
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-08-21 11:55:28 +0100

    fix: cookie selection

--- src/utils/cookieUtilities/getCookie.js
+++ src/utils/cookieUtilities/getCookie.js
@@ -1,7 +1,7 @@
 import global from 'global';
 
 const getCookie = ({ name }) => {
-  const cookies = global.document.cookie.split(';');
+  const cookies = global.document.cookie.split(';').map(cookie => cookie.trim());
 
   const foundCookie = cookies
     .find(cookie => cookie.split('=')[0] === name);
