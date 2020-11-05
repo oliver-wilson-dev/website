f2a5bda9abfa1e3479f3b40d43f52054c36f1791
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-11-05 18:40:18 +0000

    feat: introduce the projects page and the projects grid

---
+++ src/components/ProjectTile/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import styles from './index.css';
+import sharedStyles from '../../styles/shared.css';
+
+const ProjectTile = ({
+  name,
+  codeHref,
+  demoHref,
+  description,
+  technologies
+}) => (
+  <article className={styles.projectTile}>
+    <header>
+      <h3>{name}</h3>
+      {technologies && (
+      <React.Fragment>
+        <h4 className={styles.techHeader}>Technologies</h4>
+        <p className={styles.techList}>
+          {technologies.join(', ')}
+        </p>
+      </React.Fragment>
+      )}
+    </header>
+    <p>{description}</p>
+    {(codeHref || demoHref) && (
+      <footer className={styles.footer}>
+        {demoHref && <a href={demoHref} className={cn(sharedStyles.buttonLink, styles.buttonLink)} target="_blank">View demo</a>}
+        {codeHref && <a href={codeHref} className={cn(sharedStyles.buttonLink, styles.buttonLink)} target="_blank">View code</a>}
+      </footer>
+    )}
+  </article>
+);
+
+ProjectTile.propTypes = {
+  codeHref: PropTypes.string,
+  demoHref: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  technologies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default ProjectTile;
