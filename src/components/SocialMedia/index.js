49143107dcde629dcda56053d936758eb7ba9b2f
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-12-19 15:28:50 +0000

    accessibility improvements - fix contrast ratio and add aria-label to social media links(#86)

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -9,16 +9,16 @@ import Email from './email.svg';
 
 const SocialMedia = () => (
   <div className={cn(styles.socialContainer, sharedStyles.flexCenter)}>
-    <a className={styles.socialLink} href="https://www.linkedin.com/in/oliver-wilson-profile/" rel="noopener noreferrer" target="_blank">
+    <a className={styles.socialLink} href="https://www.linkedin.com/in/oliver-wilson-profile/" rel="noopener noreferrer" target="_blank" aria-label="A link to my LinkedIn page">
       <LinkedIn className={styles.socialLogo} />
     </a>
-    <a className={styles.socialLink} href="https://github.com/oliver-wilson-dev/" rel="noopener noreferrer" target="_blank">
+    <a className={styles.socialLink} href="https://github.com/oliver-wilson-dev/" rel="noopener noreferrer" target="_blank" aria-label="A link to my Github page">
       <Github className={styles.socialLogo} />
     </a>
-    <a className={styles.socialLink} href="tel:07580081310">
+    <a className={styles.socialLink} href="tel:07580081310" aria-label="My contact telephone number">
       <Phone className={styles.socialLogo} />
     </a>
-    <a className={styles.socialLink} href="mailto:contact@oliverwilson.dev">
+    <a className={styles.socialLink} href="mailto:contact@oliverwilson.dev" aria-label="My email address">
       <Email className={styles.socialLogo} />
     </a>
   </div>
