f2356716ae30f2e4ab2eed03edc92c1c5eb31693
Oliver Wilson <contact.oliver.wilson@gmail.com>
2020-12-28 20:59:15 +0000

    refactor: rename chunks

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,19 +4,19 @@ import loadable from '@loadable/component';
 const routes = {
   home: {
     path: '/',
-    component: loadable(() => import(/* webpackChunkName: "HomePage" */ '../components/HomePage'))
+    component: loadable(() => import(/* webpackChunkName: "home-page" */ '../components/HomePage'))
   },
   about: {
     path: '/about',
-    component: loadable(() => import(/* webpackChunkName: "AboutPage" */ '../components/AboutPage'))
+    component: loadable(() => import(/* webpackChunkName: "about-page" */ '../components/AboutPage'))
   },
   projects: {
     path: '/projects',
-    component: loadable(() => import(/* webpackChunkName: "ProjectsPage" */ '../components/ProjectsPage'))
+    component: loadable(() => import(/* webpackChunkName: "projects-page" */ '../components/ProjectsPage'))
   },
   notFound: {
     path: undefined,
-    component: loadable(() => import(/* webpackChunkName: "NotFoundPage" */ '../components/NotFoundPage'))
+    component: loadable(() => import(/* webpackChunkName: "notFound-page" */ '../components/NotFoundPage'))
   }
 };
 
