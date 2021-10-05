635d828d5757aa9e9407800bc1db7e9403156d09
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-10-05 20:18:09 +0100

    feat: enable swiping for section slider

--- src/components/SectionSlider/index.js
+++ src/components/SectionSlider/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import SectionTile from '../SectionTile';
@@ -11,34 +11,60 @@ import RightArrow from './right-arrow.svg';
 
 const SectionSlider = ({ children }) => {
   const [currentSelectedIndex, setCurrentSelectedIndex] = useState(0);
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
 
   const firstElementSelected = currentSelectedIndex === 0;
 
   const lastElementSelected = currentSelectedIndex === children.length - 1;
 
-  const increment = () => {
+  const increment = useCallback(() => {
     if (!lastElementSelected) {
       setCurrentSelectedIndex(currentSelectedIndex + 1);
     }
-  };
-  const decrement = () => {
+    setTouchStart(null);
+    setTouchEnd(null);
+  }, [currentSelectedIndex, lastElementSelected]);
+
+  const decrement = useCallback(() => {
     if (!firstElementSelected) {
       setCurrentSelectedIndex(currentSelectedIndex - 1);
     }
-  };
+    setTouchStart(null);
+    setTouchEnd(null);
+  }, [currentSelectedIndex, firstElementSelected]);
+
+  const onTouchStart = useCallback((e) => {
+    e.stopPropagation();
+    setTouchStart(e.targetTouches[0].clientX);
+  }, []);
+
+  const onTouchEnd = useCallback((e) => {
+    e.stopPropagation();
+    setTouchEnd(e.changedTouches[0].clientX);
+  }, []);
+
+  useEffect(() => {
+    if (touchStart !== null && touchEnd !== null) {
+      if (touchStart - touchEnd > 75) {
+        increment();
+      }
+
+      if (touchStart - touchEnd < -75) {
+        decrement();
+      }
+    }
+  }, [touchStart, touchEnd, increment, decrement]);
 
   return (
     <React.Fragment>
       <div className={styles.buttonWrapper}>
-
         <LeftArrow
           className={cn(styles.arrowBtn, {
             [styles.btnDisabled]: firstElementSelected
           })}
           onClick={decrement}
         />
-
-
         <RightArrow
           className={cn(styles.arrowBtn, {
             [styles.btnDisabled]: lastElementSelected
@@ -46,7 +72,14 @@ const SectionSlider = ({ children }) => {
           onClick={increment}
         />
       </div>
-      <div className={cn(styles.tileContainer, sectionStyles.displayBox)}>
+      <div
+        className={cn(
+          styles.tileContainer,
+          sectionStyles.displayBox
+        )}
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
+      >
         { children.map((child, index) => {
           const props = {
             border: true,
