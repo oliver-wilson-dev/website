a673ecdc1e0bbe8e44d6c54641a6bdd4e3dd6699
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-11-07 10:39:04 +0000

    feat: fallback image link placeholder

---
+++ src/components/Workplace/ProjectImage.js
@@ -0,0 +1,47 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import styles from './index.css';
+
+/*
+  Setting the image src when rendering on the client because on the server
+  no onerror event is respected.
+
+  https://stackoverflow.com/questions/51639891/detect-broken-image-when-using-hydrate-instead-of-render-from-react-dom
+*/
+
+const ProjectImage = ({ alt, src: srcProp, linkToProject }) => {
+  const [imgLoadFailed, setImgLoadFailed] = useState(false);
+  const [src, setSrc] = useState(undefined);
+
+  useEffect(() => setSrc(srcProp), []);
+
+  const onError = useCallback(() => setImgLoadFailed(true), [setImgLoadFailed]);
+
+  return (
+    <div
+      className={styles.projectImageWrapper}
+    >
+      <a href={linkToProject}>
+        <img
+          alt={alt}
+          src={src}
+          loading="lazy"
+          onError={onError}
+          className={cn({
+            [styles.image]: !imgLoadFailed,
+            [styles.imgLoadFailed]: imgLoadFailed
+          })}
+        />
+      </a>
+    </div>
+  );
+};
+
+ProjectImage.propTypes = {
+  alt: PropTypes.string,
+  src: PropTypes.string,
+  linkToProject: PropTypes.string,
+};
+
+export default ProjectImage;
